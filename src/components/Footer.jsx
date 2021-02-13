deab2c0a830f5c9d9e3e1b86bab4d3f35ce24e1a
FrancoRosa <km115.franco@gmail.com>
2021-02-12 22:58:26 -0500

    Add Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="content has-text-centered">
+          <p>
+            <strong>Weather alerts</strong>{' by '} 
+            <a href="https://jgog.in" target="_blank">Mitch</a>{' & '}
+            <a href="https://jgog.in" target="_blank">Franco</a>.
+          </p>
+          <p>
+            {'Theme: '}<strong>Bulmaswatch</strong>{' by '}
+            <a href="https://jgog.in" target="_blank">Jenil Gogari</a>.
+          </p>
+        </div>
+      </div>
+    </footer>
+  )
+};
+
+export default Footer;
